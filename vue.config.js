82adfb932effd878826ad766c4a428c9190ab132
paula Laorga <139532010+paulalaorga@users.noreply.github.com>
2024-05-12 15:13:26 +1000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/repository-name/'  // replace 'repository-name' with your GitHub repository name
+    ? '/art-history-app/'  
     : '/'
 }
