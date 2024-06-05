a622c4f5689dcd20c7fac7b380838c0e51307ff9
Paula Laorga <139532010+paulalaorga@users.noreply.github.com>
2024-06-05 07:22:11 +0000

    period page img styling not finished

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/art-history-app/" : "/",
   devServer: {
-    proxy: "http://localhost:3000",
-    allowedHosts: "all", // Allow all hosts
-  },
-  configureWebpack: {
-    devtool: "source-map", // Enable source maps for better error logging
+    allowedHosts: "all",
   },
 };
